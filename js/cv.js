929a47f7708b5000b19a553b0b0813f9ee0dbe6f
tivui <tivui64@gmail.com>
2021-05-30 00:41:09 +0200

    Premier commit

---
+++ js/cv.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll(".menu_item,.nav-link,#contact_icon");
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (e) {
+        var contenu_name = e.currentTarget.dataset.contenu;
+        document.getElementById(contenu_name).classList.add('apparition_fondue')
+        var elements = document.getElementsByClassName("contenu");
+        for (let j = 0; j < elements.length; j++) {
+            elements[j].style.display = "none";
+        }
+        document.getElementById(contenu_name).style.display = "block";
+        setTimeout(function () {
+            document.getElementById(contenu_name).classList.remove('apparition_fondue');
+          }, 0);
+    });
+}
